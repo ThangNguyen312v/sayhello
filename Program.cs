228d3ccc7301ac11bd2eb74cce47d9bcef8cc97b
ThangNguyen312v <tatthang112113@gmail.com>
2024-08-07 14:40:14 +0700

    hello

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SayHello{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Vui Lòng Cho Biết Tên:  ");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Xin Chào: " + yourName);
+        }
+    }
+}
